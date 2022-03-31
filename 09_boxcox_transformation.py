6e812acfd4fa540003ecb003bba5b741ae09c3fd
Joey C <101367256+jjac32@users.noreply.github.com>
2022-03-31 12:55:32 -0700

    Delete 09_boxcox_transformation.py

--- 09_boxcox_transformation.py
+++
@@ -1,7 +0,0 @@
-# 09 Box-Cox Transformation
-
-# Box - Cox Transform
-if input_transform_flag == 'Yes':
-    df[input_target_variable], lam = boxcox(df[input_target_variable])
-else:
-    print("No transformation")
