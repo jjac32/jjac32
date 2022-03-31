4975c44fe443042618b0d5f63c64d3c04cb8f76c
Joey C <101367256+jjac32@users.noreply.github.com>
2022-03-31 12:55:22 -0700

    Delete 07_ADF_test.py

--- 07_ADF_test.py
+++
@@ -1,9 +0,0 @@
-# 07 Stationarity test - Augmented Dickey Fuller test (ADH Test) 
-
-# ADF Test
-result = adfuller(df[input_target_variable].values, autolag='AIC')
-print(f'ADF Statistic: {result[0]}')
-print(f'p-value: {result[1]}')
-for key, value in result[4].items():
-    print('Critial Values:')
-    print(f'   {key}, {value}')
